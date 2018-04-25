3bf12950a4b74f12ec4e237d5403c7c587086a47
Bogdan Incrosnatu <incrosnatubogdan@gmail.com>
2018-04-25 14:12:58 +0300

    Remove the now ignored directory node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+    minifyCSS = require('gulp-minify-css'),
+    prefix = require('gulp-autoprefixer')
 
 var jsFiles = 'res/**/*.js',
     jsDest = 'res/js';
@@ -14,4 +16,12 @@ gulp.task('scripts', function() {
         .pipe(rename('scripts.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('styles', function(){
+    return gulp.src('res/css/*.css')
+    .pipe(concat('style.min.css'))
+    .pipe(minifyCSS())
+    .pipe(prefix('last 2 versions'))
+    .pipe(gulp.dest('res/css'))
 });
